dfe77ba8e4d2e969894f4687e25957c489fc423f
RodriiAndino <andinorodrigo03@gmail.com>
2024-03-01 12:38:06 -0300

    Add useInterviewData hook for interview components

--- src/app/interview/components/InterviewPage.tsx
+++ src/app/interview/components/InterviewPage.tsx
@@ -1,35 +1,26 @@
 'use client'
 
 import { Logo } from './Logo'
+import useInterviewData from './hooks/useInterviewData'
 import { useTrainingData } from './hooks/useTrainingData'
-import { useState, useEffect } from 'react'
-import fetchApiOpeniai from '@/app/services/fetch-api-routes-openiai'
 
 export default function InterviewPage() {
-  const [interviewData, setInterviewData] = useState(null)
   const { trainingData, interviewCharacteristics, interviewPersonality } =
     useTrainingData()
-
-  useEffect(() => {
-    async function fetchInterviewData() {
-      try {
-        const response = await fetchApiOpeniai({
-          searchParamsInterview: trainingData
-        })
-        setInterviewData(response)
-      } catch (error) {
-        console.error('Error fetching interview data:', error)
-      }
-    }
-    if (
-      trainingData.interviewType &&
-      trainingData.interviewer &&
-      trainingData.jobId
-    ) {
-      fetchInterviewData()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  const {
+    answerIsSelected,
+    answerStatus,
+    answers,
+    correctAnswer,
+    explanation,
+    handleAnswerSelection,
+    handleNewQuestion,
+    handleSubmit,
+    loading,
+    question,
+    selectedAnswer,
+    submitted
+  } = useInterviewData({ trainingData })
 
   return (
     <div className='bg-GrayL3 min-h-screen py-8 px-4 max-sm:px-0 max-sm:pt-0  max-sm:pb-12'>
@@ -39,7 +30,7 @@ export default function InterviewPage() {
           <h1 className='text-3xl font-bold mb-2 text-GrayL2'>Entrevista</h1>
         </header>
         <main>
-          <section className='mb-4'>
+          <header className='mb-4'>
             <h3 className='mb-2 text-primaryL1 font-bold text-lg'>
               Tipo de Entrevista:{' '}
               <span className='text-GrayD4 font-medium'>
@@ -58,20 +49,135 @@ export default function InterviewPage() {
                 </small>
               </span>
             </h3>
-          </section>
+          </header>
           <section className='mb-4'>
             <h3 className='font-bold mb-2 text-xl text-primary'>Enunciado</h3>
-            <p className='text-GrayD4 text-lg'></p>
+            {!question ? (
+              <div role='status' className='space-y-2.5 animate-pulse max-w-lg'>
+                <div className='flex items-center w-full'>
+                  <div className='h-2.5 rounded-full bg-primary w-32'></div>
+                  <div className='h-2.5 ms-2 rounded-full bg-primaryL1 w-24'></div>
+                  <div className='h-2.5 ms-2 rounded-full bg-primaryL1 w-full'></div>
+                </div>
+                <div className='flex items-center w-full max-w-[480px]'>
+                  <div className='h-2.5 rounded-full bg-primary w-full'></div>
+                  <div className='h-2.5 ms-2 rounded-full bg-primaryL1 w-full'></div>
+                  <div className='h-2.5 ms-2 rounded-full bg-primaryL1 w-24'></div>
+                </div>
+                <span className='sr-only'>Loading...</span>
+              </div>
+            ) : (
+              <p className='text-GrayD4 text-lg'>{question}</p>
+            )}
           </section>
           <section className='mb-4'>
             <h3 className='font-bold mb-2 text-xl text-primary'>Respuestas</h3>
-            <ul></ul>
+
+            {!question ? (
+              <div
+                role='status'
+                className='space-y-4 divide-y animate-pulse dark:divide-primaryL1 p-4 w-full'
+              >
+                <div className='flex items-center justify-between w-full pt-4'>
+                  <div className='w-full'>
+                    <div className='h-2.5  rounded-full bg-primary w-1/3 mb-2.5'></div>
+                    <div className='w-4/5 h-2 rounded-full bg-primaryL1'></div>
+                  </div>
+                  <div className='h-2.5  rounded-full bg-primaryL1 w-12'></div>
+                </div>
+                <div className='flex items-center justify-between w-full pt-4'>
+                  <div className='w-full'>
+                    <div className='h-2.5  rounded-full bg-primary w-1/3 mb-2.5'></div>
+                    <div className='w-4/5 h-2 rounded-full bg-primaryL1'></div>
+                  </div>
+                  <div className='h-2.5  rounded-full bg-primaryL1 w-12'></div>
+                </div>
+                <div className='flex items-center justify-between w-full pt-4'>
+                  <div className='w-full'>
+                    <div className='h-2.5  rounded-full bg-primary w-1/3 mb-2.5'></div>
+                    <div className='w-4/5 h-2 rounded-full bg-primaryL1'></div>
+                  </div>
+                  <div className='h-2.5  rounded-full bg-primaryL1 w-12'></div>
+                </div>
+                <div className='flex items-center justify-between w-full pt-4'>
+                  <div className='w-full'>
+                    <div className='h-2.5  rounded-full bg-primary w-1/3 mb-2.5'></div>
+                    <div className='w-4/5 h-2 rounded-full bg-primaryL1'></div>
+                  </div>
+                  <div className='h-2.5  rounded-full bg-primaryL1 w-12'></div>
+                </div>
+                <span className='sr-only'>Loading...</span>
+              </div>
+            ) : (
+              <ul>
+                {answers.map((answer, index) => (
+                  <li key={index} className='mb-2'>
+                    <button
+                      className={`w-full p-4 text-lg text-start hover:scale-[1.03] disabled:opacity-65 duration-200 ${
+                        selectedAnswer === index
+                          ? 'bg-primaryL1 text-white disabled:opacity-100'
+                          : ''
+                      }`}
+                      onClick={() => handleAnswerSelection(index)}
+                      disabled={submitted}
+                    >
+                      {answer}
+                      <hr />
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            )}
           </section>
+
+          <footer>
+            {submitted && (
+              <>
+                {answerStatus && (
+                  <h3 className='font-bold mb-2 text-xl text-primary'>
+                    Respuesta seleccionada:{' '}
+                    <span className='text-secondary'>{answerStatus}</span>
+                  </h3>
+                )}
+
+                {correctAnswer && (
+                  <article className='mb-2'>
+                    <h3 className='font-bold  text-xl text-primary'>
+                      Respuesta correcta
+                    </h3>
+                    <p className='text-GrayD4'>{correctAnswer}</p>
+                  </article>
+                )}
+
+                {explanation && (
+                  <article className='mb-4'>
+                    <h3 className='font-bold text-xl text-primary'>
+                      Explicación
+                    </h3>
+                    <p className='text-GrayD4'>{explanation}</p>
+                  </article>
+                )}
+              </>
+            )}
+          </footer>
         </main>
         <footer>
-          <button className='bg-primary text-white px-4 py-2 rounded-lg mr-2 hover:bg-secondary disabled:opacity-50'>
+          <button
+            className='bg-primary text-white px-4 py-2 rounded-lg mr-2 hover:bg-secondary disabled:opacity-50'
+            onClick={() => handleSubmit()}
+            disabled={loading || !answerIsSelected || submitted}
+          >
             Responder
           </button>
+          {submitted && (
+            <button
+              className='bg-primary text-white px-4 py-2 rounded-lg mr-2 hover:bg-secondary disabled:opacity-50'
+              onClick={() => handleNewQuestion()}
+              disabled={loading}
+            >
+              Generar nueva pregunta
+            </button>
+          )}
         </footer>
       </div>
     </div>
---
+++ src/app/interview/components/hooks/useInterviewData.tsx
@@ -0,0 +1,119 @@
+'used client'
+
+import fetchApiOpeniai from '@/app/services/fetch-api-routes-openiai'
+import { SearchParamsInterview } from '@/app/types/search-params'
+import { useEffect, useState } from 'react'
+
+export default function useInterviewData({
+  trainingData
+}: {
+  trainingData: SearchParamsInterview
+}) {
+  const [question, setQuestion] = useState<string>('')
+  const [answers, setAnswers] = useState<string[]>([])
+  const [selectedAnswer, setSelectedAnswer] = useState<number>(NaN)
+  const [answerIsSelected, setAnswerIsSelected] = useState<boolean>(false)
+  const [answerStatus, setAnswerStatus] = useState<string>('')
+  const [correctAnswer, setCorrectAnswer] = useState<string>('')
+  const [explanation, setExplanation] = useState<string>('')
+  const [submitted, setSubmitted] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const fetchData = async ({
+    trainingData
+  }: {
+    trainingData: SearchParamsInterview
+  }) => {
+    try {
+      setLoading(true)
+      const response = await fetchApiOpeniai({
+        searchParamsInterview: trainingData
+      })
+
+      const completions = response.openAIResponse
+
+      const questionStartIndex = completions.indexOf('Pregunta:')
+      const questionEndIndex = completions.indexOf('Respuestas:')
+      const answersEndIndex = completions.indexOf('Respuesta correcta')
+      const explanationIndex = completions.indexOf('Explicación:')
+
+      const questionText = completions
+        .substring(questionStartIndex, questionEndIndex)
+        .replace('Pregunta:', '')
+        .trim()
+
+      const answersText = completions
+        .substring(questionEndIndex, answersEndIndex)
+        .replace('Respuestas:', '')
+        .trim()
+        .split('\n')
+        .map((answer: string) => answer.trim())
+        .filter((answer: string) => answer !== '')
+
+      const answerCorrect = completions
+        .substring(answersEndIndex, explanationIndex)
+        .replace('Respuesta correcta:', '')
+        .trim()
+
+      const explanationText = completions
+        .substring(explanationIndex)
+        .replace('Explicación:', '')
+        .trim()
+
+      setCorrectAnswer(answerCorrect)
+      setExplanation(explanationText)
+      setQuestion(questionText)
+      setAnswers(answersText)
+      setLoading(false)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchData({ trainingData })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleAnswerSelection = (index: number) => {
+    setSelectedAnswer(index)
+    setAnswerIsSelected(true)
+  }
+
+  const handleSubmit = () => {
+    setSubmitted(true)
+
+    const formattedSelectedAnswer = answers[selectedAnswer].substring(3)
+
+    if (formattedSelectedAnswer == correctAnswer) {
+      setAnswerStatus('Correcto')
+    } else {
+      setAnswerStatus('Incorrecto')
+    }
+  }
+
+  const handleNewQuestion = () => {
+    setAnswers([])
+    setQuestion('')
+    setSelectedAnswer(NaN)
+    setAnswerStatus('')
+    setSubmitted(false)
+    setAnswerIsSelected(false)
+    fetchData({ trainingData })
+  }
+
+  return {
+    question,
+    answers,
+    selectedAnswer,
+    answerStatus,
+    explanation,
+    submitted,
+    loading,
+    answerIsSelected,
+    correctAnswer,
+    handleAnswerSelection,
+    handleSubmit,
+    handleNewQuestion
+  }
+}
